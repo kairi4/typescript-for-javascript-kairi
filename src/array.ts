4768ac7fd32c206f9bd00d8971eecfe358c7175f
kubomoto_k <gemini.blast407@gmail.com>
2022-09-04 21:12:23 +0900

    Add arrays

---
+++ src/array.ts
@@ -0,0 +1,16 @@
+export { };
+
+let numbers: number[] = [1, 2, 3];
+
+let numbers2: Array<number> = [1, 2, 3];
+let strings2: Array<string> = ["tokyo", "osaka", "kyoto"];
+
+let strings: string[] = ["type", "java", "xo"];
+
+let nijigenhairetsu: number[][] = [
+    [50, 100],
+    [100.200]
+]
+
+let hairetsu: (string | number | boolean)[] = [1, false, "java"];
+
