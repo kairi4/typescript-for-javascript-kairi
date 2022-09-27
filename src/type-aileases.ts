58fafacaf31994510719d0edb0a4555a350cd3a6
kubomoto_k <gemini.blast407@gmail.com>
2022-09-27 18:44:44 +0900

    type ailiases

---
+++ src/type-aileases.ts
@@ -0,0 +1,21 @@
+export { };
+
+type Mojiretsu = string;
+
+const fooString: string = 'hello';
+const fooMojiretsu: Mojiretsu = 'hello';
+
+const example1 = {
+    name: 'ham',
+    age: 45
+};
+
+type Profile = {
+    name: string;
+    age: number;
+}
+
+const example2: Profile = {
+    name: 'ham',
+    age: 45
+};
